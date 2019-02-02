b11ef76736d8a8f0766dc4ac530aeabfc013c40c
uis <uis9936@gmail.com>
2019-02-03 01:00:38 +0300

    Added: lua files loading, some cmake options

--- src/PluginSystem.cpp
+++ src/PluginSystem.cpp
@@ -3,6 +3,11 @@
 #include <nlohmann/json.hpp>
 #include <easylogging++.h>
 
+extern "C"{
+#include <luajit-2.0/lauxlib.h>
+#include <luajit-2.0/lualib.h>
+}
+
 #include "PluginSystem.hpp"
 
 #ifdef _POSIX_C_SOURCE
@@ -28,32 +33,46 @@
 #pragma message ("Warning: NOT THREADSAFE")
 #endif
 
+#if VERBOSE_PLUGIN_LOADING
+#define VERBOSE_JSON_PARSING 1
+#define VERBOSE_LUA_LOADING 1
+#endif
+
 namespace fs = std::experimental::filesystem;
 
 // /pugin
 // /pugin/info.json
 // /pugin/src/*.lua
-inline bool isLua(std::string a){
-    size_t len=a.length();
-    const char* str=a.c_str();
-    return str[len-4]=='.'&&str[len-3]=='l'&&str[len-2]=='u'&&str[len-1]=='a';
+static void loadLua(const fs::directory_entry &file, lua_State* lua){
+    std::string path=file.path();
+    size_t len=path.length();
+    const char* str=path.c_str();
+    if(str[len-4]=='.'&&str[len-3]=='l'&&str[len-2]=='u'&&str[len-1]=='a'){
+#if VERBOSE_LUA_LOADING
+        LOG(INFO) << "Loading lua file: " << str;
+#endif
+//        luaL_loadfile(lua, str);
+        if(luaL_dofile(lua, str)){
+            LOG(FATAL) << "Failed to load file "+file.path().string();
+        }
+    }
 }
-static void walkDir(const fs::directory_entry &dirEntry, void(*callback)(const fs::directory_entry &dirEntry), bool onFile){
+static void walkDir(const fs::directory_entry &dirEntry, void(*callback)(const fs::directory_entry &dirEntry, lua_State* lua), lua_State* lua){
 for (auto &file : fs::directory_iterator(dirEntry)) {
-    if(onFile){
+    if(lua){
         if(fs::is_directory(file)){
-            walkDir(dirEntry, callback, true);
+            walkDir(dirEntry, callback, lua);
         }else{
-            if(isLua(file.path()))
-                callback(file);
+            callback(file, lua);
         }
     }else{
         if(fs::is_directory(file)){
-            callback(file);
+            callback(file, nullptr);
         }
     }
 }
 }
+
 void* loadFile(const char* fname, size_t size){
     FILE* f=fopen(fname, "rb");
     if(f==NULL){abort();}
@@ -67,6 +86,7 @@ void* loadFile(const char* fname, size_t size){
     fclose(f);
     return p;
 }
+
 #if USE_MMAP
 #define UnloadFile(ptr, size) unloadFile(ptr, size)
 inline void unloadFile(void* ptr, size_t size){
@@ -77,35 +97,52 @@ inline void unloadFile(void* ptr){
     free(ptr);
 #endif
 }
+
 std::vector<struct ACMod> PluginSystem::mods;
 std::map<std::string, size_t> PluginSystem::modids;
 static inline auto JSONGet(std::string a, nlohmann::json j){
-#if VERBOSE_PLUGIN_LOADING
+#if VERBOSE_JSON_PARSING
     auto ref=j[a];
     if(ref==nullptr){
-        LOG(FATAL)<<"No \""+a+"\" field in "+fs::absolute(info).string();
+        LOG(FATAL)<<"No \""+a+"\" field";
     }
     return ref;
 #else
     return j[a];
 #endif
 }
-void PluginSystem::loadPlugin(const fs::directory_entry &dirEntry) {
+
+void PluginSystem::loadPlugin(const fs::directory_entry &dirEntry, lua_State* lua) {
     std::string info=dirEntry.path().string().append(SEPARATOR"acmod.info");
     size_t sz=fs::file_size(info);
     struct ACMod acm;
+    acm.status=0;
 
     void* inf=loadFile(info.c_str(), sz);
 
     nlohmann::json j=nlohmann::json::parse((char*)inf);
     UnloadFile(inf, sz);
 
-    acm.name=JSONGet("name", j).get<std::string>();
-    acm.status=0b1;
-    modids.insert(std::pair<std::string, size_t>(JSONGet("modid", j).get<std::string>(), mods.size()));
+    std::string modid=JSONGet("modid", j).get<std::string>();
+    LOG(INFO) << "Loading plugin: " << (acm.name=JSONGet("name", j).get<std::string>()).c_str();
+    if(JSONGet("type", j).get<std::string>()=="lua"){
+        if(lua==nullptr){
+            acm.lua=lua_open();
+            luaL_openlibs(acm.lua);
+        }else{
+            acm.lua=lua;
+        }
+        walkDir(dirEntry, loadLua, acm.lua);
+        acm.status=LUA;
+    }else{
+        acm.lua=nullptr;
+    }
+    acm.status|=LOADED;
+    modids.insert(std::pair<std::string, size_t>(modid, mods.size()));
     mods.push_back(acm);
 }
 void initPluginSystem(){
-    walkDir(fs::directory_entry("./mods/"), PluginSystem::loadPlugin, false);
-    LOG(INFO) << PluginSystem::mods.size() << " plugins loaded";
+    LOG(INFO) << "Initializing UPluginSystem";
+    walkDir(fs::directory_entry("./mods/"), PluginSystem::loadPlugin, nullptr);
+    LOG(INFO) << PluginSystem::mods.size() << " plugin(s) loaded";
 }
--- src/PluginSystem.hpp
+++ src/PluginSystem.hpp
@@ -2,21 +2,26 @@
 #include <string>
 #include <map>
 #include <experimental/filesystem>
+extern "C" {
+#include <luajit-2.0/luajit.h>
+}
 enum ACMStatus{
     LOADED=0b1,
     LUA=0b10,
     HAS_ASSETS=0b100,
+    NATIVE=0b1000
 };
 struct ACMod
 {
     uint8_t status;
 //    uint16_t id;
     std::string name;
+    lua_State* lua;
 };
 void initPluginSystem();
 class PluginSystem{
 public:
-    static void loadPlugin(const std::experimental::filesystem::directory_entry &dirEntry);
+    static void loadPlugin(const std::experimental::filesystem::directory_entry &dirEntry, lua_State* l);
     static std::vector<struct ACMod> mods;
     static std::map<std::string, size_t> modids;
 };
