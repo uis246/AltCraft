a2e0747a227b29aef404b72e3f32fc73d69c428a
UIS <uis9936@gmail.com>
2020-10-19 23:59:29 +0300

    Added PlaySound()

--- src/Audio.hpp
+++ src/Audio.hpp
@@ -2,6 +2,13 @@
 
 #include "Vector.hpp"
 
+#ifdef AC_AUDIO_OPENAL
+#include "al.h"
+
+//typedef ALuint sound_t;
+typedef ALuint buffer_t;
+#endif
+
 namespace Audio {
 	void Init();
 	void Deinit() noexcept;
@@ -9,4 +16,8 @@ namespace Audio {
 	void UpdateListener(Vector3<float> direction, Vector3<float> up, Vector3<float> pos, Vector3<float> speed) noexcept;
 
 	void LoadOGG(void *mapping, size_t mlen, void **bufferPtr, int *channels, int *freq, size_t *blen) noexcept;
+
+        void PlaySound(Vector3<float> pos, buffer_t buffer, bool relative = false, bool loop = false) noexcept;
+
+        void GCSounds() noexcept;
 }
--- src/audio/OpenAL.cpp
+++ src/audio/OpenAL.cpp
@@ -4,19 +4,20 @@
 #include <string>
 #include <easylogging++.h>
 
-#include "al.h"
 #include "alc.h"
 
 ALCdevice *device;
 ALCcontext *context;
 
-bool alCheckError_(const char *file, int line) noexcept;
+std::vector<ALuint> sources;
+
+ALenum alCheckError_(const char *file, int line) noexcept;
 #define alCheckError() alCheckError_(__FILE__, __LINE__)
 
-bool alcCheckError_(const char *file, int line) noexcept;
+ALCenum alcCheckError_(const char *file, int line) noexcept;
 #define alcCheckError(dev) alcCheckError_(dev, __FILE__, __LINE__)
 
-bool alCheckError_(const char *file, int line) noexcept {
+ALenum alCheckError_(const char *file, int line) noexcept {
 	ALenum errorCode = alGetError();
 	if (errorCode != AL_NO_ERROR) {
 		std::string error;
@@ -38,12 +39,11 @@ bool alCheckError_(const char *file, int line) noexcept {
 				break;
 		}
 		LOG(ERROR) << "OpenAL error: " << error << " at " << file << ":" << line;
-		return true;
-	} else
-		return false;
+	}
+	return errorCode;
 }
 
-bool alcCheckError_(ALCdevice *dev, const char *file, int line) noexcept {
+ALCenum alcCheckError_(ALCdevice *dev, const char *file, int line) noexcept {
 	ALCenum errorCode = alcGetError(dev);
 	if (errorCode != ALC_NO_ERROR) {
 		std::string error;
@@ -65,10 +65,9 @@ bool alcCheckError_(ALCdevice *dev, const char *file, int line) noexcept {
 				break;
 		}
 		LOG(ERROR) << "OpenAL context error: " << error << " at " << file << ":" << line;
-		return true;
 		assert(0);
-	} else
-		return false;
+	}
+	return errorCode;
 }
 
 void Audio::Deinit() noexcept {
@@ -89,9 +88,38 @@ void Audio::UpdateListener(Vector3<float> direction, Vector3<float> up, Vector3<
 	ALfloat listPos[6] = {direction.x, direction.y, direction.z, up.x, up.y, up.z};
 
 	alListener3f(AL_POSITION, pos.x, pos.y, pos.z);
-	alCheckError();
+//	alCheckError();
 	alListener3f(AL_VELOCITY, vel.x, vel.y, vel.z);
-	alCheckError();
+//	alCheckError();
 	alListenerfv(AL_ORIENTATION, listPos);
+//	alCheckError();
+}
+
+void Audio::PlaySound(Vector3<float> pos, buffer_t buffer, bool relative, bool loop) noexcept {
+	ALuint source;
+	alGenSources(1, &source);
+	if(alCheckError()==AL_OUT_OF_MEMORY)
+		GCSounds();
+	alSourcei(source, AL_SOURCE_RELATIVE, relative);
+	alSource3f(source, AL_POSITION, pos.x, pos.y, pos.z);
+	alSourcei(source, AL_BUFFER, buffer);
+	alSourcei(source, AL_LOOPING, loop);
+	alSourcePlay(source);
 	alCheckError();
+}
+
+//For background
+//alSourcei(id, AL_DIRECT_CHANNELS_SOFT, 1)
+
+std::vector<ALuint> toDelete;
+void Audio::GCSounds() noexcept {
+	toDelete.resize(sources.size());
+	size_t cnt=0;
+	for(ALuint source : sources) {
+		ALint state;
+		alGetSourcei(source, AL_SOURCE_STATE, &state);
+		if(state==AL_STOPPED)
+			toDelete[cnt++]=source;
+	}
+	alDeleteSources(cnt, toDelete.data());
 }
