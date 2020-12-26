ec3276b14357fe9ebd5ad22f88de471cc9ea449f
UIS <uis9936@gmail.com>
2020-12-26 22:58:23 +0300

    WIP

---
+++ src/RendererUI.cpp
@@ -0,0 +1,95 @@
+#include "RendererUI.hpp"
+
+RendererUI::RendererUI() {
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(BUFCOUNT, VBOs);
+}
+RendererUI::~RendererUI() {
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(BUFCOUNT, VBOs);
+}
+
+void RendererUI::PrepareRender() noexcept {
+	if(!dirtry && !permanentDirty)
+		return;//All buffers up to date
+
+	struct LayerStore *layer;
+	layer = layers.data();
+
+	//Generate geometry
+	struct RenderBuffer rbuf;
+	for(size_t i = min; i < max; i++) {
+		layer[i].info.renderUpdate(&rbuf, layer[i].argument);
+	}
+	elementType = GL_UNSIGNED_SHORT;
+	elements = rbuf.index.size();
+
+	//Upload buffers
+	glBindBuffer(GL_VERTEX_ARRAY, VBOs[0]);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VBOs[1]);
+	//TODO: buffer re-specification
+	glBufferData(GL_VERTEX_ARRAY, rbuf.buffer.size() * sizeof(float), rbuf.buffer.data(), GL_STREAM_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, elements * sizeof(GLushort), rbuf.index.data(), GL_STREAM_DRAW);
+	glBindBuffer(GL_VERTEX_ARRAY, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+void RendererUI::Render() noexcept {//Just draw
+	glDrawElements(GL_TRIANGLES, elements, elementType, 0);
+}
+
+
+
+
+void RendererUI::UpdateRenderInfo() noexcept {
+	dirtry = true;
+	permanentDirty = false;
+
+	struct LayerStore *layer = layers.data();
+	size_t count = layers.size();
+
+	if(!count) {
+		//No UI to render
+		//That's strange, but let's skip
+		dirtry = false;
+		min = 0;
+		max = 0;
+		return;
+	}
+
+	//Get layer render interval
+	max = count;
+	for(size_t i = count - 1;; i--) {
+		min = i;
+		currentLayer = layer[i].info.layer;
+		permanentDirty |= layer[i].permadirt;
+		if(currentLayer == MENU_ONLY)
+			break;
+		if(i == 0)
+			break;
+	}
+}
+
+void RendererUI::PopLayer() noexcept {
+	layers.pop_back();
+
+	UpdateRenderInfo();
+}
+
+void RendererUI::PushLayer(struct LayerInfo &layer, void *customArg, bool alwaysUpdate) {
+	struct LayerStore newLayer;
+	newLayer.info = layer;
+	newLayer.argument = customArg;
+	newLayer.permadirt = alwaysUpdate;
+	layers.push_back(newLayer);
+
+	UpdateRenderInfo();
+}
+
+void RendererUI::Redraw() noexcept {
+	dirtry = true;
+}
+
+GLuint RendererUI::getVAO() noexcept {
+	return VAO;
+}
---
+++ src/RendererUI.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "Platform.hpp"
+
+//#include <string>
+#include <vector>
+
+#include <GL/glew.h>
+
+enum MouseButtons {
+	LEFT = 0,
+	RIGHT,
+	MIDDLE,
+	AUX1,
+	AUX2,
+	BUTTONCOUNT
+};
+enum ModKeysMask {
+	SHIFT = 0x01,
+	META = 0x02,
+	CTRL = 0x04,
+	ALT = 0x08,
+};
+enum Layer {
+	GLOBAL_OVERLAY = 0,//Render alwaya
+	WORLD_OVERLAY,//Render over world
+	MENU_WORLD_OVERLAY,//Same as WORLD_OVERLAY, but with mouse ungrab
+	MENU_ONLY,//Stops world rendering, skips lower layer of menu
+	MENU_OVERLAY//Stops world rendering, doesn't skip lower layer of menu
+};
+
+struct AC_API IOState {
+	bool mouse[BUTTONCOUNT];
+	uint8_t modifierKeys;
+};
+
+struct AC_API RenderBuffer {//TODO
+	std::vector<GLushort> index;
+	std::vector<float> buffer;
+	//vec2 - position
+	//vec4 - color multiplier
+	//vec3 - UvLayer
+	//9 floats = 36 bytes
+};
+
+struct AC_API LayerInfo {
+	void (*onEvent)(struct IOState *state, void *custom);
+	void (*renderUpdate)(struct RenderBuffer *buf, void *custom);
+	enum Layer layer;
+};
+
+class RendererUI final {
+	enum buffers {
+		BUFVERTS = 0,
+		BUFELEMENTS,
+		BUFCOUNT
+	};
+	struct LayerStore final {
+		struct LayerInfo info;
+		void *argument;
+		bool permadirt;
+	};
+	//Some GL info
+	GLuint VAO, VBOs[BUFCOUNT];
+	GLsizei elements;
+	GLenum elementType;
+
+	//Layers
+	std::vector<LayerStore> layers;
+
+	//Redraw info
+	bool dirtry = true;
+	bool permanentDirty = false;
+	size_t min = 0, max = 0;
+	enum Layer currentLayer = GLOBAL_OVERLAY;
+
+	AC_INTERNAL void UpdateRenderInfo() noexcept;
+public:
+	RendererUI();
+	~RendererUI();
+
+	//Add UI layer
+	AC_API void PushLayer(struct LayerInfo &layer, void *customArg = nullptr, bool alwaysUpdate = false);
+	//Remove UI layer
+	AC_API void PopLayer() noexcept;
+	//Mark geometry ditry
+	AC_API void Redraw() noexcept;
+	AC_INTERNAL void Render() noexcept;
+	AC_INTERNAL void PrepareRender() noexcept;
+	AC_INTERNAL GLuint getVAO() noexcept;
+};
